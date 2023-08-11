2f556f4bb19d86a7630c0b4fc82d240830fdbb93
alesolano <asolanopf@gmail.com>
2023-08-11 13:54:34 +0200

    add some order. make test pass

--- app/helpers/local_time_helper.rb
+++ app/helpers/local_time_helper.rb
@@ -33,7 +33,7 @@ module LocalTimeHelper
     time = utc_time(time)
 
     options, date_format = extract_options_and_value(options, :format)
-    date_format = find_date_formats(date_format)
+    date_format = find_date_format(date_format)
 
     options[:data] ||= {}
     options[:data].merge! local: :time, format: date_format
@@ -78,6 +78,8 @@ module LocalTimeHelper
       end
     end
 
+    ## TIME ##
+
     def find_time_formats(format12)
       if format12.is_a?(Symbol)
         # If format12 is a Symbol (like :db), we try to find the corresponding String format.
@@ -85,44 +87,6 @@ module LocalTimeHelper
       else
         [ format12.presence || LocalTime.default_time_format, nil ]
       end
-    end
-
-    def find_date_formats(date_format)
-      if date_format.is_a?(Symbol)
-        find_date_formats_by_name(date_format)
-      else
-        date_format.presence || LocalTime.default_date_format
-      end
-    end
-
-    def find_date_formats_by_name(name)
-      if use_i18n_date_formats?(name)
-        find_i18n_date_formats(name)
-      elsif use_ruby_date_formats?(name)
-        find_ruby_date_formats(name)
-      else
-        LocalTime.default_date_format
-      end
-    end
-
-    def use_i18n_date_formats?(name)
-      i18n_date_format(name).present?
-    end
-
-    def i18n_date_format(name)
-      I18n.t("date.formats.#{name}", default: [ :"time.formats.#{name}", "" ]).presence
-    end
-
-    def find_i18n_date_formats(name)
-      i18n_date_format(name)
-    end
-
-    def use_ruby_date_formats?(name)
-      ruby_preferred_format(name, prefer: :date).present?
-    end
-
-    def find_ruby_date_formats(name)
-      ruby_preferred_format(name, prefer: :date)
     end
 
     def find_time_formats_by_name(name)
@@ -155,14 +119,6 @@ module LocalTimeHelper
       ruby_preferred_format(name).present?
     end
 
-    def ruby_preferred_format(name, prefer: :time)
-      if prefer == :time
-        Time::DATE_FORMATS.with_indifferent_access[name] || Date::DATE_FORMATS.with_indifferent_access[name]
-      else
-        Date::DATE_FORMATS.with_indifferent_access[name] || Time::DATE_FORMATS.with_indifferent_access[name]
-      end
-    end
-
     def find_ruby_time_formats(name)
       format12 = ruby_preferred_format(name)
 
@@ -170,6 +126,56 @@ module LocalTimeHelper
         [ LocalTime.default_time_format, nil ]
       else
         [ format12, ruby_preferred_format("#{name}_24h") ]
+      end
+    end
+
+    ## DATE ##
+
+    def find_date_format(date_format)
+      if date_format.is_a?(Symbol)
+        find_date_format_by_name(date_format)
+      else
+        date_format.presence || LocalTime.default_date_format
+      end
+    end
+
+    def find_date_format_by_name(name)
+      if use_i18n_date_format?(name)
+        find_i18n_date_format(name)
+      elsif use_ruby_date_format?(name)
+        find_ruby_date_format(name)
+      else
+        LocalTime.default_time_format
+      end
+    end
+
+    def use_i18n_date_format?(name)
+      i18n_date_format(name).present?
+    end
+
+    def i18n_date_format(name)
+      I18n.t("date.formats.#{name}", default: [ :"time.formats.#{name}", "" ]).presence
+    end
+
+    def find_i18n_date_format(name)
+      i18n_date_format(name)
+    end
+
+    def use_ruby_date_format?(name)
+      ruby_preferred_format(name, prefer: :date).present?
+    end
+
+    def find_ruby_date_format(name)
+      ruby_preferred_format(name, prefer: :date)
+    end
+
+    ## COMMON ##
+
+    def ruby_preferred_format(name, prefer: :time)
+      if prefer == :time
+        Time::DATE_FORMATS.with_indifferent_access[name] || Date::DATE_FORMATS.with_indifferent_access[name]
+      else
+        Date::DATE_FORMATS.with_indifferent_access[name] || Time::DATE_FORMATS.with_indifferent_access[name]
       end
     end
 end
