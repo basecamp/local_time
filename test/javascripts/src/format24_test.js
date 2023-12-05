bfceead1e182b38509b912e524f3cdf5e92186fa
decaffeinate <brunoprietog@hey.com>
2023-12-05 12:55:29 -0300

    decaffeinate: Convert calendar_date.coffee and 24 other files to JS

--- test/javascripts/src/format24_test.js
+++ test/javascripts/src/format24_test.js
@@ -1,94 +1,116 @@
-import LocalTime from "local_time"
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
+ */
+import LocalTime from "local_time";
 
-{addTimeEl, assert, defer, getText, testAsync, testGroup, getTitle} = LocalTime.TestHelpers
-{config} = LocalTime
+const {addTimeEl, assert, defer, getText, testAsync, testGroup, getTitle} = LocalTime.TestHelpers;
+const {config} = LocalTime;
 
-testGroup "format24 (24h time)", ->
-  testAsync "default behavior", (done) ->
-    now = moment()
+testGroup("format24 (24h time)", function() {
+  testAsync("default behavior", function(done) {
+    const now = moment();
 
-    el = addTimeEl format: "%-l:%M%P", format24: "%H:%M", datetime: now.toISOString()
-    defer ->
-      assert.equal getText(el), now.format("h:mma")
-      done()
+    const el = addTimeEl({format: "%-l:%M%P", format24: "%H:%M", datetime: now.toISOString()});
+    return defer(function() {
+      assert.equal(getText(el), now.format("h:mma"));
+      return done();
+    });
+  });
 
-  testAsync "turned off", (done) ->
-    now = moment()
-    originalFormat24 = config.useFormat24
-    config.useFormat24 = false
+  testAsync("turned off", function(done) {
+    const now = moment();
+    const originalFormat24 = config.useFormat24;
+    config.useFormat24 = false;
 
-    el = addTimeEl format: "%-l:%M%P", format24: "%H:%M", datetime: now.toISOString()
-    defer ->
-      assert.equal getText(el), now.format("h:mma")
-      config.useFormat24 = originalFormat24
-      done()
+    const el = addTimeEl({format: "%-l:%M%P", format24: "%H:%M", datetime: now.toISOString()});
+    return defer(function() {
+      assert.equal(getText(el), now.format("h:mma"));
+      config.useFormat24 = originalFormat24;
+      return done();
+    });
+  });
 
-  testAsync "turned on", (done) ->
-    now = moment()
-    originalFormat24 = config.useFormat24
-    config.useFormat24 = true
+  testAsync("turned on", function(done) {
+    const now = moment();
+    const originalFormat24 = config.useFormat24;
+    config.useFormat24 = true;
 
-    el = addTimeEl format: "%-l:%M%P", format24: "%H:%M", datetime: now.toISOString()
-    defer ->
-      assert.equal getText(el), now.format("HH:mm")
-      config.useFormat24 = originalFormat24
-      done()
+    const el = addTimeEl({format: "%-l:%M%P", format24: "%H:%M", datetime: now.toISOString()});
+    return defer(function() {
+      assert.equal(getText(el), now.format("HH:mm"));
+      config.useFormat24 = originalFormat24;
+      return done();
+    });
+  });
 
-  testAsync "fallback for missing data-format24 values", (done) ->
-    now = moment()
-    originalFormat24 = config.useFormat24
-    config.useFormat24 = true
+  testAsync("fallback for missing data-format24 values", function(done) {
+    const now = moment();
+    const originalFormat24 = config.useFormat24;
+    config.useFormat24 = true;
 
-    el = addTimeEl format: "%-l:%M%P", datetime: now.toISOString()
-    defer ->
-      assert.equal getText(el), now.format("h:mma")
-      config.useFormat24 = originalFormat24
-      done()
+    const el = addTimeEl({format: "%-l:%M%P", datetime: now.toISOString()});
+    return defer(function() {
+      assert.equal(getText(el), now.format("h:mma"));
+      config.useFormat24 = originalFormat24;
+      return done();
+    });
+  });
 
-  testAsync "turned off for relative time elements", (done) ->
-    ago = moment().subtract("days", 5)
-    originalFormat24 = config.useFormat24
-    config.useFormat24 = false
+  testAsync("turned off for relative time elements", function(done) {
+    const ago = moment().subtract("days", 5);
+    const originalFormat24 = config.useFormat24;
+    config.useFormat24 = false;
 
-    el = addTimeEl type: "time-ago", datetime: ago.toISOString()
-    defer ->
-      assert.equal getText(el), "#{ago.format("dddd")} at #{ago.format("h:mma")}"
-      config.useFormat24 = originalFormat24
-      done()
+    const el = addTimeEl({type: "time-ago", datetime: ago.toISOString()});
+    return defer(function() {
+      assert.equal(getText(el), `${ago.format("dddd")} at ${ago.format("h:mma")}`);
+      config.useFormat24 = originalFormat24;
+      return done();
+    });
+  });
 
-  testAsync "turned on for relative time elements", (done) ->
-    ago = moment().subtract("days", 5)
-    originalFormat24 = config.useFormat24
-    config.useFormat24 = true
+  testAsync("turned on for relative time elements", function(done) {
+    const ago = moment().subtract("days", 5);
+    const originalFormat24 = config.useFormat24;
+    config.useFormat24 = true;
 
-    el = addTimeEl type: "time-ago", datetime: ago.toISOString()
-    defer ->
-      assert.equal getText(el), "#{ago.format("dddd")} at #{ago.format("HH:mm")}"
-      config.useFormat24 = originalFormat24
-      done()
+    const el = addTimeEl({type: "time-ago", datetime: ago.toISOString()});
+    return defer(function() {
+      assert.equal(getText(el), `${ago.format("dddd")} at ${ago.format("HH:mm")}`);
+      config.useFormat24 = originalFormat24;
+      return done();
+    });
+  });
 
-  testAsync "element title when turned off", (done) ->
-    ago = moment().subtract("days", 5)
-    originalFormat24 = config.useFormat24
-    config.useFormat24 = false
+  testAsync("element title when turned off", function(done) {
+    const ago = moment().subtract("days", 5);
+    const originalFormat24 = config.useFormat24;
+    config.useFormat24 = false;
     
-    el = addTimeEl type: "time-ago", datetime: ago.toISOString()
-    defer ->
-      regex = new RegExp(ago.format('MMMM D, YYYY [at] h:mma') + " (\\w{3,4}|UTC[+-]d+)")
-      assert.ok regex.test(getTitle(el)), "'#{getTitle(el)}' doesn't look correct, it should match regex #{regex}"
-      config.useFormat24 = originalFormat24
-      done()
+    const el = addTimeEl({type: "time-ago", datetime: ago.toISOString()});
+    return defer(function() {
+      const regex = new RegExp(ago.format('MMMM D, YYYY [at] h:mma') + " (\\w{3,4}|UTC[+-]d+)");
+      assert.ok(regex.test(getTitle(el)), `'${getTitle(el)}' doesn't look correct, it should match regex ${regex}`);
+      config.useFormat24 = originalFormat24;
+      return done();
+    });
+  });
 
 
-  testAsync "element title when turned on", (done) ->
-    ago = moment().subtract("days", 5)
-    originalFormat24 = config.useFormat24
-    config.useFormat24 = true
+  return testAsync("element title when turned on", function(done) {
+    const ago = moment().subtract("days", 5);
+    const originalFormat24 = config.useFormat24;
+    config.useFormat24 = true;
     
-    el = addTimeEl type: "time-ago", datetime: ago.toISOString()
-    defer ->
-      regex = new RegExp(ago.format('MMMM D, YYYY [at] HH:mm') + " (\\w{3,4}|UTC[+-]d+)")
-      assert.ok regex.test(getTitle(el)), "'#{getTitle(el)}' doesn't look correct, it should match regex #{regex}"
-      config.useFormat24 = originalFormat24
-      done()
+    const el = addTimeEl({type: "time-ago", datetime: ago.toISOString()});
+    return defer(function() {
+      const regex = new RegExp(ago.format('MMMM D, YYYY [at] HH:mm') + " (\\w{3,4}|UTC[+-]d+)");
+      assert.ok(regex.test(getTitle(el)), `'${getTitle(el)}' doesn't look correct, it should match regex ${regex}`);
+      config.useFormat24 = originalFormat24;
+      return done();
+    });
+  });
+});
 
