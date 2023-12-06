0e6b31a9d5845989f052a686c28ff8db9fa036bb
Bruno Prieto <brunoprietog@hey.com>
2023-12-06 02:31:19 -0300

    decaffeinate: Sanity-check the JavaScript

--- test/javascripts/src/index.js
+++ test/javascripts/src/index.js
@@ -1,12 +1,10 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
-import "moment";
-import "sinon";
+import "moment"
+import "sinon"
 
-import "./test_helpers";
-import "./format24_test";
-import "./i18n_test";
-import "./local_time_test";
-import "./relative_date_test";
-import "./strftime_test";
-import "./time_ago_test";
+import "./test_helpers"
+import "./format24_test"
+import "./i18n_test"
+import "./local_time_test"
+import "./relative_date_test"
+import "./strftime_test"
+import "./time_ago_test"
