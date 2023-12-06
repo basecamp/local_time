8a010804b481c0d4cda24caf7cbcfdd1b141b3b3
Jose Farias <jose@37signals.com>
2023-12-05 20:16:29 -0600

    Clean up tests

--- test/javascripts/integration/element_mutation_test.js
+++ test/javascripts/integration/element_mutation_test.js
@@ -8,42 +8,42 @@ test.beforeEach(async ({ page }) => {
 test("test changing the element's text content re-processes the element", async ({ page }) => {
   await page.click("button#morph")
 
+  await assertSingleUpdate(page, "#one")
   assert.equal(await page.textContent("#one"), "December 4, 2024")
-  assert.equal(await page.getAttribute("#one", "data-updates"), "1")
 })
 
 test("test changing the element's datatime attribute re-processes the element", async ({ page }) => {
   await page.click("button#change-datetime")
 
+  await assertSingleUpdate(page, "#one")
   assert.equal(await page.textContent("#one"), "December 4, 2015")
-  assert.equal(await page.getAttribute("#one", "data-updates"), "1")
 })
 
 test("test changing the element's data-local attribute re-processes the element", async ({ page }) => {
   await page.click("button#change-relative")
 
+  await assertSingleUpdate(page, "#one")
   assert.equal(await page.textContent("#one"), "on Dec 4, 2024")
-  assert.equal(await page.getAttribute("#one", "data-updates"), "1")
 })
 
 test("test changing the element's data-format attribute re-processes the element", async ({ page }) => {
   await page.click("button#change-format")
 
+  await assertSingleUpdate(page, "#one")
   assert.equal(await page.textContent("#one"), "Dec 4")
-  assert.equal(await page.getAttribute("#one", "data-updates"), "1")
 })
 
 test("test changing the element's data-format24 attribute re-processes the element", async ({ page }) => {
   await page.click("button#change-format24")
 
+  await assertSingleUpdate(page, "#one")
   assert.equal(await page.textContent("#one"), "December 4")
-  assert.equal(await page.getAttribute("#one", "data-updates"), "1")
 })
 
 test("test changing an unlisted attribute does not re-process the element", async ({ page }) => {
   await page.click("button#change-foo")
 
-  assert.equal(await page.getAttribute("#one", "data-updates"), "0")
+  await assertNoUpdates(page, "#one")
 })
 
 test("test removing the element disconnects its mutation observer", async ({ page }) => {
@@ -64,3 +64,11 @@ test("test elements are only observed once", async ({ page }) => {
   const isSingleObserver = await page.evaluate(() => LocalTime.getController().observedElements.size === 1)
   assert.isTrue(isSingleObserver, "There should only be one MutationObserver")
 })
+
+async function assertNoUpdates(page, selector) {
+  assert.equal(await page.getAttribute(selector, "data-updates"), "0")
+}
+
+async function assertSingleUpdate(page, selector) {
+  assert.equal(await page.getAttribute(selector, "data-updates"), "1")
+}
